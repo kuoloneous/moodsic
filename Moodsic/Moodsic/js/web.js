be39fd9fbbcd25987ea1c94dbc2750c5cafbbeca
jchand <jchand@usc.edu>
2014-02-01 04:35:38 -0800

    Tweaked scrolling

--- Moodsic/Moodsic/js/web.js
+++ Moodsic/Moodsic/js/web.js
@@ -1,16 +1,25 @@
 ﻿$(document).ready(function () {
+
+
     $('.thumbnailView').click(function (event) {
         $(this).css("border", "10px solid black").css("box-sizing", "border-box");
     });
+
     $("div#scrollView").smoothDivScroll({
-    	autoScrollingMode: "onStart",
+    	autoScrollingMode: "",
     	hotSpotScrolling: false,
     	touchScrolling: true,
+
+    	startAtElementId: "b",
+		scrollToAnimationDuration: 2000,
+		scrollToEasingFunction: "easeOutBounce",
 
     	mousewheelScrollingStep: 1,
     	mousewheelScrolling: "horizontal",
     	easingAfterMouseWheelScrolling: true,
-    	easingAfterMouseWheelScrollingFunction: "easeOutQuint",
-    	easingAfterMouseWheelScrollingDuration: 500
+    	easingAfterMouseWheelScrollingFunction: "easeOutQuad",
+    	easingAfterMouseWheelScrollingDuration: 500,
     });
+
+    $("div#scrollView").smoothDivScroll("scrollToElement", "first");
 });
