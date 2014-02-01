69ae034703f74cb3197fff4fe1a8bccd6a57cd6b
jchand <jchand@usc.edu>
2014-02-01 03:48:31 -0800

    Added smooth scrolling

--- Moodsic/Moodsic/js/web.js
+++ Moodsic/Moodsic/js/web.js
@@ -2,4 +2,15 @@
     $('.thumbnailView').click(function (event) {
         $(this).css("border", "10px solid black").css("box-sizing", "border-box");
     });
+    $("div#scrollView").smoothDivScroll({
+    	autoScrollingMode: "onStart",
+    	hotSpotScrolling: false,
+    	touchScrolling: true,
+
+    	mousewheelScrollingStep: 1,
+    	mousewheelScrolling: "horizontal",
+    	easingAfterMouseWheelScrolling: true,
+    	easingAfterMouseWheelScrollingFunction: "easeOutQuint",
+    	easingAfterMouseWheelScrollingDuration: 500
+    });
 });
